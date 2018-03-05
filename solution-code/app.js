113766a4beccec3859d40f02d4edb5c084a8b655
Victor Arza <papu.arza@gmail.com>
2018-03-05 12:01:44 +0100

    replace ejs by hbs

--- solution-code/app.js
+++ solution-code/app.js
@@ -1,23 +1,25 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const hbs = require('hbs');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+
+const index = require('./routes/index');
+const users = require('./routes/users');
 const celebrities = require('./routes/celebrities');
 const movies = require('./routes/movies');
 
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/mongoose-movies-development');
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set('view engine', 'hbs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -34,7 +36,7 @@ app.use('/', movies);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
+  const err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
